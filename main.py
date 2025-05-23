755e76733dfd257997bc039727697c8000b38aa6
zachary62 <zhuang333@wisc.edu>
2025-05-22 21:57:33 -0400

    update pattern

--- main.py
+++ main.py
@@ -9,16 +9,30 @@ dotenv.load_dotenv()
 # Default file patterns
 DEFAULT_INCLUDE_PATTERNS = {
     "*.py", "*.js", "*.jsx", "*.ts", "*.tsx", "*.go", "*.java", "*.pyi", "*.pyx",
-    "*.c", "*.cc", "*.cpp", "*.h", "*.md", "*.rst", "Dockerfile",
-    "Makefile", "*.yaml", "*.yml",
+    "*.c", "*.cc", "*.cpp", "*.h", "*.md", "*.rst", "*Dockerfile",
+    "*Makefile", "*.yaml", "*.yml",
 }
 
 DEFAULT_EXCLUDE_PATTERNS = {
-    "assets/*", "data/*", "examples/*", "images/*", "public/*", "static/*", "temp/*",
-    "docs/*", 
-    "venv/*", ".venv/*", "*test*", "tests/*", "docs/*", "examples/*", "v1/*",
-    "dist/*", "build/*", "experimental/*", "deprecated/*", "misc/*", 
-    "legacy/*", ".git/*", ".github/*", ".next/*", ".vscode/*", "obj/*", "bin/*", "node_modules/*", "*.log"
+    "assets/*", "data/*", "images/*", "public/*", "static/*", "temp/*",
+    "*docs/*",
+    "*venv/*",
+    "*.venv/*",
+    "*test*",
+    "*tests/*",
+    "*examples/*",
+    "v1/*",
+    "*dist/*",
+    "*build/*",
+    "*experimental/*",
+    "*deprecated/*",
+    "*misc/*",
+    "*legacy/*",
+    ".git/*", ".github/*", ".next/*", ".vscode/*",
+    "*obj/*",
+    "*bin/*",
+    "*node_modules/*",
+    "*.log"
 }
 
 # --- Main Function ---
